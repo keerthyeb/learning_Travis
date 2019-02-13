2f2f12685d2ecfa27b4cb29fad308ac7f571a301
Keerthy E B <44020061+keerthyeb@users.noreply.github.com>
2019-02-13 13:23:47 +0530

    modified:   test.js

--- test.js
+++ test.js
@@ -1,5 +1,5 @@
 const assert = require("assert");
-const { sum, difference } = require("../main");
+const { sum, difference } = require("./main");
 
 describe("add", function() {
   it("should sum of two numbers", function() {
